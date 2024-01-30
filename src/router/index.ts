1f14cea849056db7000ec3e0d4e631d3f113dfc7
Adejuwon Oshadipe <bibire95@gmail.com>
2024-01-30 08:28:51 +0100

    Fix: Netlify page not found on reload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
